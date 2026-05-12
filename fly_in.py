0a7edf6d1613ca04a6dee45d1ad251a5b4c2a41e
otahiri <chucy0066@gmail.com>
2026-05-12 16:10:27 +0100

    feat: added the inital stuff for the pydantic validation  for the data

--- fly_in.py
+++ fly_in.py
@@ -1,14 +1,17 @@
+import sys
 from parsing import ParsingError, Graph
 
 
 def main():
-    graph = Graph()
+    lines = list()
+    with open(sys.argv[1], "r") as map:
+        lines = [
+            line.strip("\n")
+            for line in map
+            if len(line.strip()) and not line.strip().startswith("#")
+        ]
+    graph = Graph(lines)
     print(graph.drone_num)
-    print(graph.start.groupdict().get("meta_data"))
-    for hub in graph.hubs:
-        print(hub.groupdict().get("meta_data"))
-    print(graph.finish)
-    print(graph.connections)
 
 
 if __name__ == "__main__":
--- parsing.py
+++ parsing.py
@@ -1,21 +1,12 @@
+from os import error
 import re
-import sys
 from typing import Any, List
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, Field, field_validator
 
 
 class ParsingError(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
-
-
-lines: list = []
-with open(sys.argv[1], "r") as map:
-    lines = [
-        line.strip("\n")
-        for line in map
-        if len(line.strip()) and not line.strip().startswith("#")
-    ]
 
 
 class Line(BaseModel):
@@ -38,44 +29,78 @@ class Line(BaseModel):
 
 
 class Graph:
-    hub_regex = re.compile(
-        r"^hub:\s+(?P<name>\S+)\s(?P<x>-?\d+)\s(?P<y>-?\d+)"
-        r"(?:\s+\[(?P<meta_data>.*?)\])?$"
-    )
-    start_regex = re.compile(
-        r"^start_hub:\s+(?P<name>\S+)\s+(?P<x>-?\d+)\s+(?P<y>-?\d+)"
-        r"(?:\s+\[(?P<meta_data>.*?)\])?$"
-    )
-    goal_regex = re.compile(
-        r"^end_hub:\s+(?P<name>\S+)\s+(?P<x>-?\d+)\s+(?P<y>-?\d+)"
-        r"(?:\s+\[(?P<meta_data>.*?)\])?$"
-    )
-    drone_num_regex = re.compile(r"^nb_drones:\s+(?P<num>-?\d+)")
-    connections_regex = re.compile(
-        r"connection:\s+(?P<node_a>\S+)-(?P<node_b>\S+)"
-    )
+
+    start: Line = Field()
+    finish: Line = Field()
+    drone_num: int = Field()
+    hubs: List[Line] = Field()
+    connections: List[tuple] = Field()
 
     def __init__(self, lines: List[str]) -> None:
-        self.start: Line | None
-        self.finish: Line | None
-        self.drone_num: int | 
-        self.hubs: List[Line] = []
-        self.connections: List[dict] = []
-        for i, line in enumerate(lines):
+        hub_regex = re.compile(
+            r"^hub:\s+(?P<name>\S+)\s(?P<x>-?\d+)\s(?P<y>-?\d+)"
+            r"(?:\s+\[(?P<meta_data>.*?)\])?$"
+        )
+        start_regex = re.compile(
+            r"^start_hub:\s+(?P<name>\S+)\s+(?P<x>-?\d+)\s+(?P<y>-?\d+)"
+            r"(?:\s+\[(?P<meta_data>.*?)\])?$"
+        )
+        goal_regex = re.compile(
+            r"^end_hub:\s+(?P<name>\S+)\s+(?P<x>-?\d+)\s+(?P<y>-?\d+)"
+            r"(?:\s+\[(?P<meta_data>.*?)\])?$"
+        )
+        drone_num_regex = re.compile(r"^nb_drones:\s+(?P<num>-?\d+)")
+        connections_regex = re.compile(
+            r"connection:\s+(?P<node_a>\S+)-(?P<node_b>\S+)"
+        )
+
+        idx = 0
+        parsed_start: Line | None = None
+        parsed_finish: Line | None = None
+        parsed_drone_num: int = -1
+        parsed_hubs: List[Line] = []
+        parsed_connections: List[tuple] = []
+        for line in lines:
+            idx += 1
             line = line.strip()
-            if not line:
+            if not line or line.startswith("#"):
                 continue
-            if match := Graph.start_regex.search(line):
-                if self.start:
+            if match := start_regex.search(line):
+                if parsed_start:
                     raise ParsingError(f"duplicated start hub {line}")
-                self.start = Line.model_validate(match.groupdict())
-                lines.pop(i)
+                parsed_start = Line.model_validate(match.groupdict())
+                parsed_hubs.append(Line.model_validate(match.groupdict()))
 
-            elif match := Graph.goal_regex.search(line):
-                if self.finish:
+            elif match := goal_regex.search(line):
+                if parsed_finish:
                     raise ParsingError(f"duplicated goal hub {line}")
-                self.finish = Line.model_validate(match.groupdict())
-                lines.pop(i)
+                parsed_finish = Line.model_validate(match.groupdict())
+                parsed_hubs.append(Line.model_validate(match.groupdict()))
 
-            elif match := Graph.drone_num_regex.search(line):
-                if self.drone
+            elif match := drone_num_regex.search(line):
+                if parsed_drone_num > -1:
+                    raise ParsingError(f"duplicated drone number {line}")
+                try:
+                    parsed_drone_num = int(match.groupdict().get("num", -1))
+                except ValueError:
+                    raise ParsingError(f"invalid drone number {line}")
+                if parsed_drone_num <= 0:
+                    raise ParsingError(f"invalid drone number {line}")
+            elif match := hub_regex.search(line):
+                parsed_hubs.append(Line.model_validate(match.groupdict()))
+            elif match := connections_regex.search(line):
+                parsed_connections.append(
+                    (
+                        match.groupdict().get("node_a"),
+                        match.groupdict().get("node_b"),
+                    )
+                )
+                a, b = parsed_connections[-1]
+                if a not in [hub.name for hub in parsed_hubs] or b not in [
+                    hub.name for hub in parsed_hubs
+                ]:
+                    raise ParsingError(f"invalid connection {line}")
+            else:
+                break
+        if idx < len(lines):
+            raise ParsingError(f"invalid map additional lines {lines[idx::]}")
